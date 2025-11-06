cd0b7301e9fbd1c29452795d237e6fa0c99900be
lsylvestre <loic.sylvestre@outlook.fr>
2025-11-06 23:47:52 +0100

    fix deadcode elimination

--- eclat-compiler/src/Eclat/compile/middle_end/normalize/move_down_gfun_under_exec_and_par.ml
+++ eclat-compiler/src/Eclat/compile/middle_end/normalize/move_down_gfun_under_exec_and_par.ml
@@ -11,6 +11,7 @@ module Deadcode_elimination = struct
     let exception Found in
     let rec aux xs = function
     | E_var x ->
+        if SMap.mem x xs then () else
         if x = var then raise Found
     | E_match(e1,hs,eo) ->
         aux xs e1;
@@ -49,7 +50,8 @@ module Deadcode_elimination = struct
 
   let rec deadcode_elim = function
     | E_letIn(P_var f,tyf,(E_fix(h,(p,(ty,tyB),e1)) as phi),e2) ->
-        if (f <> h) then deadcode_elim (E_letIn(P_var f,(*tyf*)Types.new_ty_unknown(),(E_fix(f,(p,(Types.new_ty_unknown(),Types.new_tyB_unknown())(*(ty,tyB)*),Ast_subst.subst_e h (E_var f) e1))),e2)) else
+        if (f <> h) then deadcode_elim (E_letIn(P_var f,(*tyf*)Types.new_ty_unknown(),
+                                           (E_fix(f,(p,(Types.new_ty_unknown(),Types.new_tyB_unknown())(*(ty,tyB)*), Ast_subst.subst_e h (E_var f) e1))),e2)) else
         if in_fv f e2 then E_letIn(P_var f,(*tyf*)Types.new_ty_unknown(),deadcode_elim phi, deadcode_elim e2) else deadcode_elim e2
     | e -> Ast_mapper.map deadcode_elim e
 
@@ -63,21 +65,30 @@ end
 
 let rec map_under_exec_and_par e =
   let decl env e =
-    List.fold_right (fun (x,v) e -> E_letIn(P_var x, Types.new_ty_unknown(), v,e)) (List.rev env) e
+    List.fold_left (fun e (x,v) -> 
+        if not(SMap.mem x@@Free_vars.fv e) (* todo: avoid the traversal of [e] for each name [x] *)
+        then e 
+        else E_letIn(P_var x, Types.new_ty_unknown(), v,e)
+    ) e env
   in
   let rec aux env e =
     match e with
     | E_letIn(P_var f,ty,(E_fix(g,(p,(ty1,tyB2),e1))),e2) ->
         let e1' = aux env e1 in (* assume [env] disjoint of [g] and [p] *)
+        (** todo: avoid fresh unknowns and loose of type annotation. 
+            The problem is about instantiation of type variables in presence 
+            of polymorphic recursive function (which should be monomorphized before) **)
         let phi' = E_fix(g,(p,(* (ty1,tyB2)*)(Types.new_ty_unknown(),Types.new_tyB_unknown()),(* aux env*) e1')) in   (* was [aux env e1'] before: a bug ? *)
         E_letIn(P_var f,Types.new_ty_unknown(),phi',aux ((f,phi')::env) e2)
     | E_par(es) ->
         E_par(List.map (fun e -> map_under_exec_and_par (decl env e)) es)
-    (* | E_reg((p,e1),e0,l) ->
-        E_reg((p,map_under_exec_and_par (decl env e1)), aux env e0,l) *)
     | E_exec(e1,e0,eo,l) ->
-        E_exec(map_under_exec_and_par (decl env e1), aux env e0, Option.map (aux env) eo, l)
-    | e -> Ast_mapper.map (aux env) e
+        E_exec(map_under_exec_and_par (decl env e1), aux env e0, Option.map (aux env) eo, l) 
+        (* why not ((decl env (aux env e1))) ? *)
+    | e -> 
+       (** no special case for [reg] 
+           because this never occurs under an exec **)
+       Ast_mapper.map (aux env) e
   in aux [] e
 
 
@@ -85,6 +96,6 @@ let move_down_gfun_under_exec_and_par_pi pi =
   let main = map_under_exec_and_par pi.main in
   {pi with main}
   |> Deadcode_elimination.deadcode_elimination_pi
-    (* deadcode elimination is needed
-       because [map_under_exec_and_par] introduces a lot of
-       unused function declarations *)
+    (** deadcode elimination is absolutely needed
+        because [map_under_exec_and_par] introduces a lot of
+        unused function declarations **)
